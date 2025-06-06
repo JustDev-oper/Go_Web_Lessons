99918c69e2220a35f13c033cd7f60a586049a8d1
dead.code <itoppro11@gmail.com>
2025-06-06 11:27:25 +0300

    Урок 6
    Добавление данных через сайт и вывод постов на главную страницу
    https://youtu.be/IcEKRmzcXdU?si=8AIxN4E6t0pmKXyY

--- News_site/main.go
+++ News_site/main.go
@@ -1,24 +1,93 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 	"html/template"
 	"net/http"
 )
 
+type Article struct {
+	Id       uint16
+	Title    string
+	Anons    string
+	FullText string
+}
+
 func index(w http.ResponseWriter, r *http.Request) {
+	var articles []Article
+
 	temp, err := template.ParseFiles("News_site/templates/index.html", "News_site/templates/header.html", "News_site/templates/footer.html")
 	if err != nil {
 		fmt.Fprintf(w, err.Error())
 	}
-	temp.ExecuteTemplate(w, "index", nil)
+
+	db, err := sql.Open("mysql", "root:Ar11042008@tcp(127.0.0.1:3306)/golang")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	res, err := db.Query("SELECT * FROM `articles`")
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+
+	for res.Next() {
+		var post Article
+		err = res.Scan(&post.Id, &post.Title, &post.Anons, &post.FullText)
+		if err != nil {
+			fmt.Fprintf(w, err.Error())
+		}
+		articles = append(articles, post)
+	}
+
+	temp.ExecuteTemplate(w, "index", articles)
+}
+
+func create(w http.ResponseWriter, r *http.Request) {
+	temp, err := template.ParseFiles("News_site/templates/create.html", "News_site/templates/header.html", "News_site/templates/footer.html")
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+	temp.ExecuteTemplate(w, "create", nil)
+}
+
+func saveArticle(w http.ResponseWriter, r *http.Request) {
+	title := r.FormValue("title")
+	anons := r.FormValue("anons")
+	fullText := r.FormValue("full_text")
+
+	if title == "" || anons == "" || fullText == "" {
+		fmt.Fprintf(w, "Не все данные заполнены")
+	} else {
+
+		db, err := sql.Open("mysql", "root:Ar11042008@tcp(127.0.0.1:3306)/golang")
+		if err != nil {
+			panic(err)
+		}
+		defer db.Close()
+
+		// Установка данных
+		insert, err := db.Query(fmt.Sprintf("INSERT INTO `articles` (`title`, `anons`, `full_text`) VALUES ('%s', '%s', '%s')", title, anons, fullText))
+		if err != nil {
+			panic(err)
+		}
+		defer insert.Close()
+
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+	}
 }
 
 func handleFunc() {
 	http.HandleFunc("/", index)
+	http.HandleFunc("/create", create)
+	http.HandleFunc("/save_article", saveArticle)
 	http.ListenAndServe(":8080", nil)
 }
 
 func main() {
+
 	handleFunc()
 }
